d17348dee13c47de773cfa8fdcf40f43c69e80e8
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2023-04-19 21:37:47 +0700

    update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,23 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gallery</title>
+    <title>Homepage</title>
 </head>
 
 <body>
-    @extends('layouts.app')
-    @section('body')
-        <div class="container">
-            <h1>About</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo aliquam expedita id sint fugiat inventore amet
-                obcaecati. Iusto, itaque adipisci, fuga eius earum mollitia quidem accusantium nemo error obcaecati
-                repellendus sapiente, in minima at ab doloribus quaerat voluptatem! Inventore beatae voluptatum quae ex eos
-                cupiditate culpa praesentium natus soluta voluptate aperiam unde totam odit possimus asperiores ullam
-                voluptas, repellat eum similique iure dolor! Ullam accusantium eaque id illum nemo sequi porro iusto, iure
-                modi recusandae. Nemo placeat, ut, delectus at quidem tempore ea recusandae fuga veritatis accusantium
-                facere corrupti qui. Odio, vitae eos. Voluptas illum totam sed beatae neque explicabo!</p>
-        </div>
-    @endsection
+    <x-app>
+        @section('body')
+            <div class="container">
+                <h1>About</h1>
+                <x-card title="Homepage" subtitle="This is Homepage">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nemo numquam, ex fuga possimus, nulla
+                    incidunt facilis sit sunt omnis officia, modi adipisci odio nobis perspiciatis assumenda exercitationem.
+                    Similique explicabo quaerat qui quasi quia consectetur ea assumenda sapiente. Est iste quaerat quis
+                    sapiente
+                    cupiditate fuga incidunt error consectetur nemo omnis.
+                </x-card>
+            </div>
+        @endsection
+    </x-app>
 </body>
 
 </html>
